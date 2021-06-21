e997b9a8eb2b425e644607a12d375049df506613
shivani <shikushwah@bestpeers.com>
2021-06-21 16:03:48 +0530

    to find the area of reactangle

---
+++ area_rect.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+puts 'Enter value of length and breadth'
+length = gets.chomp.to_i
+breadth = gets.chomp.to_i
+area = length * breadth
+puts "Area of rectangle is: #{area}"
